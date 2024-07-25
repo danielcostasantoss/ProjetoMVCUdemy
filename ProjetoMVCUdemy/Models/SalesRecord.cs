311e5c40f828feb7a9470ade4388604836cc9c18
daniel costa santos <dancsantos99@icloud.com>
2024-07-25 15:33:58 -0300

    Outras entidades e segunda migration.

---
+++ ProjetoMVCUdemy/Models/SalesRecord.cs
@@ -0,0 +1,25 @@
+﻿using ProjetoMVCUdemy.Models.Enums;
+
+namespace ProjetoMVCUdemy.Models
+{
+    public class SalesRecord
+    {
+        public int Id { get; set; }
+        public DateTime Data { get; set; }
+        public double Quantia { get; set; }
+
+        public SaleStatus Status { get; set; }
+        public Seller Seller { get; set; }
+
+        public SalesRecord() { }
+
+        public SalesRecord(int id, DateTime data, double quantia, SaleStatus status, Seller seller)
+        {
+            Id = id;
+            Data = data;
+            Quantia = quantia;
+            Status = status;
+            Seller = seller;
+        }
+    }
+}
---
+++ ProjetoMVCUdemy/Models/Seller.cs
@@ -0,0 +1,39 @@
+﻿namespace ProjetoMVCUdemy.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public double SalarioBase { get; set; }
+
+        public Department Department { get; set; }
+        public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
+
+        public Seller() { }
+
+        public Seller(int id, string nome, string email, DateTime dataNascimento, double salarioBase, Department department)
+        {
+            Id = id;
+            Nome = nome;
+            Email = email;
+            DataNascimento = dataNascimento;
+            SalarioBase = salarioBase;
+            Department = department;
+        }
+
+        public void AddSales(SalesRecord sr)
+        {
+            Sales.Add(sr);
+        }
+        public void RemoveSales(SalesRecord sr)
+        {
+            Sales.Remove(sr);
+        }
+        public double TotalSales(DateTime inicial, DateTime final)
+        {
+            return Sales.Where(sr => sr.Data >= inicial && sr.Data <= final).Sum(sr => sr.Quantia);
+        }
+    }
+}
