705c22dba6f1d4d806bd24edcfe6c635186b5054
daniel costa santos <dancsantos99@icloud.com>
2024-07-25 16:18:38 -0300

    Sellers Controller

--- ProjetoMVCUdemy/Program.cs
+++ ProjetoMVCUdemy/Program.cs
@@ -6,19 +6,30 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ProjetoMVCUdemyContext>(options =>
     options.UseMySql(builder.Configuration.GetConnectionString("ProjetoMVCUdemyContext"),
     new MySqlServerVersion(new Version(8, 0, 21))));
-//options.UseMySql(builder.Configuration.GetConnectionString("ProjetoMVCUdemyContext"), builder.MigrationAssembly("ProjetoMVCUdemy")));
 
 // Add services to the container.
+builder.Services.AddScoped<SeedingService>();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+using (var scope = app.Services.CreateScope())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    var seedingService = scope.ServiceProvider.GetRequiredService<SeedingService>();
+
+    // Configura o pipeline de requisição HTTP.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseDeveloperExceptionPage();
+        seedingService.Seed();
+    }
+    else
+    {
+        app.UseExceptionHandler("/Home/Error");
+        // O valor padrão do HSTS é 30 dias. Você pode querer mudar isso para cenários de produção, veja https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
 }
 
 app.UseHttpsRedirection();
