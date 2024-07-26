7a2825a7a969d208f9f142538b5aef2701952270
daniel costa santos <dancsantos99@icloud.com>
2024-07-26 15:59:34 -0300

    Atualização do Seller e exceção de serviço personalizado

--- ProjetoMVCUdemy/Controllers/SellersController.cs
+++ ProjetoMVCUdemy/Controllers/SellersController.cs
@@ -3,6 +3,7 @@ using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.Blazor;
 using ProjetoMVCUdemy.Models;
 using ProjetoMVCUdemy.Models.ViewModels;
 using ProjetoMVCUdemy.Services;
+using ProjetoMVCUdemy.Services.Exceptions;
 
 namespace ProjetoMVCUdemy.Controllers
 {
@@ -55,7 +56,7 @@ namespace ProjetoMVCUdemy.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Delete (int id)
+        public IActionResult Delete(int id)
         {
             _sellerService.Remove(id);
             return RedirectToAction(nameof(Index));
@@ -76,6 +77,47 @@ namespace ProjetoMVCUdemy.Controllers
                 }
             }
             return View(seller);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+            {
+                {
+                    return NotFound();
+                }
+            }
+            List<Department> departments = _departmentService.FindAll();
+            SellerFormViewModel viewModel = new SellerFormViewModel { Seller = obj, Departments = departments };
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Seller seller)
+        {
+            if (id != seller.Id)
+            {
+                return BadRequest();
+            }
+            try
+            {
+                _sellerService.Update(seller);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (NotFoundException)
+            {
+                return NotFound();
+            }
+            catch(DbConcurrencyException)
+            {
+                return BadRequest();
+            }
         }
     }
 }
--- ProjetoMVCUdemy/Services/SellerService.cs
+++ ProjetoMVCUdemy/Services/SellerService.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.EntityFrameworkCore;
 using ProjetoMVCUdemy.Data;
 using ProjetoMVCUdemy.Models;
+using ProjetoMVCUdemy.Services.Exceptions;
 
 namespace ProjetoMVCUdemy.Services
 {
@@ -33,6 +35,23 @@ namespace ProjetoMVCUdemy.Services
             var obj = _context.Seller.Find(id);
             _context.Seller.Remove(obj);
             _context.SaveChanges();
+        }
+
+        public void Update(Seller obj)
+        {
+            if (!_context.Seller.Any(x => x.Id == obj.Id))
+            {
+                throw new DirectoryNotFoundException("Id not found");
+            }
+            try
+            {
+                _context.Update(obj);
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException e)
+            {
+                throw new DbConcurrencyException(e.Message);
+            }
         }
     }
 }
