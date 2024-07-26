ccf512527d6ccbc146a5b0c3c3fd0f4bb5bec531
daniel costa santos <dancsantos99@icloud.com>
2024-07-26 12:05:17 -0300

    ForeignKey não nula (Integridade referencial)

--- ProjetoMVCUdemy/Models/Seller.cs
+++ ProjetoMVCUdemy/Models/Seller.cs
@@ -9,6 +9,7 @@
         public double SalarioBase { get; set; }
 
         public Department Department { get; set; }
+        public int DepartmentId { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
         public Seller() { }
