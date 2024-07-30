48fc95e524be0848047b7ac642e8e53c755445a1
daniel costa santos <dancsantos99@icloud.com>
2024-07-30 10:20:35 -0300

    Implementando pesquisa simples

---
+++ ProjetoMVCUdemy/Services/SalesRecordService.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjetoMVCUdemy.Data;
+using ProjetoMVCUdemy.Models;
+
+namespace ProjetoMVCUdemy.Services
+{
+    public class SalesRecordService 
+    {
+        private readonly ProjetoMVCUdemyContext _context;
+
+        public SalesRecordService(ProjetoMVCUdemyContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<SalesRecord>> FindByDateAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.SalesRecord select obj;
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Data >= minDate.Value);
+            }
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.Data <= maxDate.Value);
+            }
+            return await result
+                .Include(x => x.Seller)
+                .Include(x => x.Seller.Department)
+                .OrderByDescending(x => x.Data)
+                .ToListAsync();
+        }
+    }
+}
