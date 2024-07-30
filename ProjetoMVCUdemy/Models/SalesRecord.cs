e57dca1f6c802d3b7b453a5699d542b23b12f689
daniel costa santos <dancsantos99@icloud.com>
2024-07-30 10:33:41 -0300

    Implementando pesquisa de agrupamento

--- ProjetoMVCUdemy/Models/SalesRecord.cs
+++ ProjetoMVCUdemy/Models/SalesRecord.cs
@@ -1,11 +1,16 @@
 ﻿using ProjetoMVCUdemy.Models.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace ProjetoMVCUdemy.Models
 {
     public class SalesRecord
     {
         public int Id { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0: dd/MM/yyyy}} ")]
         public DateTime Data { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:F2}" )]
         public double Quantia { get; set; }
 
         public SaleStatus Status { get; set; }
--- ProjetoMVCUdemy/Services/SalesRecordService.cs
+++ ProjetoMVCUdemy/Services/SalesRecordService.cs
@@ -30,5 +30,24 @@ namespace ProjetoMVCUdemy.Services
                 .OrderByDescending(x => x.Data)
                 .ToListAsync();
         }
+
+        public async Task<List<IGrouping<Department, SalesRecord>>> FindByDateGroupingAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.SalesRecord select obj;
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Data >= minDate.Value);
+            }
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.Data <= maxDate.Value);
+            }
+            return await result
+                .Include(x => x.Seller)
+                .Include(x => x.Seller.Department)
+                .OrderByDescending(x => x.Data)
+                .GroupBy(x => x.Seller.Department)
+                .ToListAsync();
+        }
     }
 }
